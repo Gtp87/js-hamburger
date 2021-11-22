64b7e5213890df1bb2a461bedc2af50f340c372b
Gtp87 <fabio.giannitrapani@gmail.com>
2021-11-22 18:12:52 +0100

    asggiunta chiusura menu hamburger

--- main.js
+++ main.js
@@ -1,6 +1,8 @@
 // seleziono le variabili
 const hamburger = document.querySelector('.hamburger-menu');
 const openHamburger = document.querySelector('.header-right a i');
+const closeHamburger = document.querySelector('.close');
+
 
 
 // apro menu hamburger
@@ -8,3 +10,7 @@ openHamburger.addEventListener('click', function(){
     hamburger.classList.add('active');
 });
 
+// chiudo menu hamburger
+closeHamburger.addEventListener('click', function(){
+    hamburger.classList.remove('active');
+});
